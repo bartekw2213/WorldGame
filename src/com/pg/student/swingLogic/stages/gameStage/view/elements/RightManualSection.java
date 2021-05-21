4623a381690fac5fcc65eeaba8ba44f8c23dce59
bartekw2213 <bartekw2213@gmail.com>
2021-05-21 12:28:33 +0200

    Created save button

--- src/com/pg/student/swingLogic/stages/gameStage/view/elements/RightManualSection.java
+++ src/com/pg/student/swingLogic/stages/gameStage/view/elements/RightManualSection.java
@@ -1,11 +1,23 @@
 package com.pg.student.swingLogic.stages.gameStage.view.elements;
 
+import com.pg.student.swingLogic.colors.ColorPalette;
+import com.pg.student.swingLogic.uiElements.MyButton;
+
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class RightManualSection extends VerticalSection {
+    private MyButton saveButton;
+
     public RightManualSection(int width, Color color) {
         super(width, color);
         AddLabels();
+        AddSaveButton();
+    }
+
+    public void AddSaveListener(ActionListener listener) {
+        saveButton.addActionListener(listener);
     }
 
     @Override
@@ -15,5 +27,11 @@ public class RightManualSection extends VerticalSection {
         super.AddLabel("Lewo", "src/com/pg/student/images/manual/left.png");
         super.AddLabel("Prawo", "src/com/pg/student/images/manual/right.png");
         super.AddLabel("SuperMoc", "src/com/pg/student/images/manual/s-letter.png");
+    }
+
+    private void AddSaveButton() {
+        this.saveButton = new MyButton("Zapisz", (int)(PANEL_WIDTH * 0.75), (int)(PANEL_WIDTH * 0.4),
+                Color.decode(ColorPalette.DARK_GREEN), Color.decode(ColorPalette.YELLOW));
+        this.add(this.saveButton);
     }
 }
